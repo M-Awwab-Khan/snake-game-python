09170185216b47ec135027d58573309d2871fd4b
Awwab <mawwabkhank2006@gmail.com>
2024-02-05 09:28:00 +0500

    go the snake moving

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('My Snake Game')
+screen.tracer(0)
 
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
+segments = []
+
 for position in starting_positions:
     new_segment = Turtle("square")
     new_segment.color('white')
+    new_segment.penup()
     new_segment.goto(position)
+    segments.append(new_segment)
+
+while True:
+    screen.update()
+    time.sleep(0.1)
+    for segment in segments:
+        segment.forward(20)
 
 screen.exitonclick()
