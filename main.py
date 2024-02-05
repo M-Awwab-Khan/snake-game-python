960eaae9879221857d36c0a9f87711f1c71a5a9c
Awwab <mawwabkhank2006@gmail.com>
2024-02-05 09:22:35 +0500

    made snake body using 3 squares

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('My Snake Game')
 
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color('white')
+    new_segment.goto(position)
+
 screen.exitonclick()
