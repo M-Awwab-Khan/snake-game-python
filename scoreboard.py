a6c4e22929f76d58fa7d6467c1e4cc24558f2e20
Awwab <mawwabkhank2006@gmail.com>
2024-02-05 10:20:16 +0500

    made scoreboard class inherited from turtle

---
+++ scoreboard.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.color('white')
+        self.penup()
+        self.score = 0
+        self.goto(0, 270)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score}", align='center', font=('Courier', 20, 'normal'))
+
+    def increase_score(self):
+        self.score += 1
