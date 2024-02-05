f7fb6ddad48fef2695ddcf4b1a037e6cd295ea39
Awwab <mawwabkhank2006@gmail.com>
2024-02-05 16:25:48 +0500

    replaced gameover function with reset

--- scoreboard.py
+++ scoreboard.py
@@ -9,16 +9,22 @@ class ScoreBoard(Turtle):
         self.color('white')
         self.penup()
         self.score = 0
+        self.high_score = 0
         self.goto(0, 270)
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
 
-    def gameover(self):
-        self.goto(0, 0)
-        self.write('Game Over', align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+    # def gameover(self):
+    #     self.goto(0, 0)
+    #     self.write('Game Over', align=ALIGNMENT, font=FONT)
