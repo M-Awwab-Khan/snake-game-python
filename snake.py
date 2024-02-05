7805a829c91ae17e2c42a97f357f8a22720298c9
Awwab <mawwabkhank2006@gmail.com>
2024-02-05 09:57:44 +0500

    restricted opposite direction snake movements using heading

--- snake.py
+++ snake.py
@@ -2,6 +2,10 @@ from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
     def __init__(self):
@@ -23,13 +27,17 @@ class Snake:
         self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        self.head.setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(90)
 
     def down(self):
-        self.head.setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(270)
 
     def left(self):
-        self.head.setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(180)
 
     def right(self):
-        self.head.setheading(360)
+        if self.head.heading() != LEFT:
+            self.head.setheading(0)
