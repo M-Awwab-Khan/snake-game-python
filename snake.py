c3788523756e40bd941f7ae4864f7bfcbf006666
Awwab <mawwabkhank2006@gmail.com>
2024-02-05 16:26:09 +0500

    implemented reset snake function

--- snake.py
+++ snake.py
@@ -47,3 +47,10 @@ class Snake:
 
     def extend(self):
         self.add_segment(self.segments[-1].position())
+
+    def reset(self):
+        for segment in self.segments:
+            segment.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
